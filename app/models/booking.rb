997f1b7e80180a215030d56730a470894eb57faa
Marcos Suarez <marcossuarezr88@gmail.com>
2023-03-30 14:30:02 +0200

    model car and booking updated

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,14 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :car
+
+  validates :starts_at, :ends_at, presence: true
+
+  before_destroy :destroy_user_bookings
+
+  private
+
+  def destroy_user_bookings
+    Booking.where(user_id: user_id).destroy_all
+  end
 end
--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,8 @@
 class Car < ApplicationRecord
   belongs_to :user
+
+  has_many :bookings
+
+  validates :brand, :model, :address, presence: true
+  validates :price_per_day, numericality: { greater_than: 0 }
 end
