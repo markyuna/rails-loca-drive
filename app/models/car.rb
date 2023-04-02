99ba7c90cdd7b442597e80efd0ddcb4d14770b6b
Emmanuel Dedryver <marcossuarezr88@gmail.com>
2023-04-02 17:34:07 +0200

    updating models

--- app/models/car.rb
+++ app/models/car.rb
@@ -6,13 +6,15 @@ class Car < ApplicationRecord
   has_many :bookings
   has_one_attached :photo
 
-  validates :brand, :model, :address, presence: true
+  validates :brand, presence: true
+  validates :model, presence: true
+  validates :year_of_production, presence: true
+  validates :address, presence: true
+
   validates :price_per_day, numericality: { greater_than: 0 }
 
   include PgSearch::Model
-  pg_search_scope :search_by_city_address,
-    against: [ :city, :address ],
-    using: {
-      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+  pg_search_scope :search_by_city_address, against: [ :address ], using: {
+    tsearch: { prefix: true } # <-- now `superman batm` will return something!
     }
 end
